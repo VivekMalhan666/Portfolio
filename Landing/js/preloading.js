893535398cb6d7ca423c7edc6616280209ca4a04
mr. Robot <robot@localhost.localdomain>
2020-08-10 02:45:29 +0530

    Accessability issue resolved

--- Landing/js/preloading.js
+++ Landing/js/preloading.js
@@ -1,13 +1,13 @@
 var preload = document.getElementById("preloader");
 var loading = 0;
-var id = setInterval(frame ,30);
+var id = setInterval(frame ,20);
 function frame(){
-    if(loading == 40){
+    if(loading == 20){
         preload.remove();
         clearInterval(id);
     }else{
         loading = loading + 1;
-        if(loading == 35){
+        if(loading == 25){
             preload.style.animation = "fadeout 1s ease";
         }
     }
